264b583af55f447efc165ff4c412601b68388103
Santhosh.V <santhosh.v4@cognizant.com>
2024-01-10 15:44:11 +0530

    Moved script to App.js file and created CSS file.

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement(
+  "h1",
+  { className: "test-header" },
+  "Welcome to my react app"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - Adding H1 using Javascript
 - Adding H1 using react CDN
+- Moved script to App.js file from html file. Created CSS file.
